ab6a7ae3f84ba858fcc214ae5b91143d74296d65
YonatanAdam <yonatansh07@gmail.com>
2026-03-01 19:02:17 +0200

    added booking dialog

---
+++ driving-lessons/BookingDialog.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+using Model;
+
+namespace driving_lessons;
+
+public partial class BookingDialog : Window {
+    public BookingDialog(Student student, Teacher teacher)
+    {
+        InitializeComponent();
+    }
+}
