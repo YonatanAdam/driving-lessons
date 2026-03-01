35f7ef66a2ef22cbbf580953edcd915659c51ace
YonatanAdam <yonatansh07@gmail.com>
2026-03-01 18:53:52 +0200

    add navigation system, login and my profile

--- driving-lessons/MainWindow.xaml.cs
+++ driving-lessons/MainWindow.xaml.cs
@@ -11,13 +11,37 @@ using System.Windows.Shapes;
 
 namespace driving_lessons;
 
+public enum UserType {
+    None,
+    Student,
+    Teacher
+}
+
 /// <summary>
 /// Interaction logic for MainWindow.xaml
 /// </summary>
 public partial class MainWindow : Window
 {
+    public UserType currentUserType = UserType.None;
     public MainWindow()
     {
         InitializeComponent();
+        // MainFrame.NavigationService.Navigated += NavigationService_Navigated;
+        MainFrame.Navigate(new LoginPage(this));
+    }
+
+    private void NavigationService_Navigated(object sender, NavigationEventArgs e)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void SignOut()
+    {
+        MainFrame.Navigate(new LoginPage(this));
+
+        while (MainFrame.CanGoBack)
+        {
+            MainFrame.RemoveBackEntry();
+        }
     }
 }
