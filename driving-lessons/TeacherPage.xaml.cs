f7b55b12cad5affb2d247b951d86aab8924dfc3f
YonatanAdam <yonatansh07@gmail.com>
2026-03-01 19:01:43 +0200

    added teacher dashboard and profile window

---
+++ driving-lessons/TeacherPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using Model;
+using ViewModel;
+
+namespace driving_lessons;
+
+public partial class TeacherPage : Page {
+    private Teacher _teacher;
+    private User _user;
+    public TeacherPage(User user, Teacher teacher)
+    {
+        InitializeComponent();
+        _user = user;
+        _teacher = teacher;
+        
+        DisplayInfo();
+        BioTextBox.Text = _teacher.Bio ?? "";
+    }
+
+    private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+    {
+        var newBio = BioTextBox.Text;
+        
+        TeacherDB teacherDb = new TeacherDB();
+        teacherDb.UpdateBio(_teacher, newBio);
+        
+        BioTextBox.Text =  newBio;
+    }
+
+    private void DisplayInfo()
+    {
+        IdTextBlock.Text = _teacher.Id.ToString();
+        UserIdTextBlock.Text = _teacher.UserId.ToString();
+        NameTextBlock.Text = _user.Name;
+        BioTextBlock.Text = _teacher.Bio ?? "No bio available";
+    }
+}
---
+++ driving-lessons/TeacherProfileWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+using Model;
+
+namespace driving_lessons;
+
+public partial class TeacherProfileWindow : Window {
+    private Teacher _teacher;
+    public TeacherProfileWindow(Teacher teacher)
+    {
+        InitializeComponent();
+        _teacher = teacher;
+
+        this.DataContext = _teacher;
+    }
+
+    private void LoadTeacherData()
+    {
+        
+    }
+}
